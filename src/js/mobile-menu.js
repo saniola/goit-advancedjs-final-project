c9efceea1b39ad6d458140f5fbaeb0509e0838e4
dariia_babii <dariia.babii02@gmail.com>
2024-07-05 21:29:07 +0200

    changed styles and js

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -4,17 +4,20 @@
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
 
+  const openMenu = () => {
+    mobileMenu.classList.add('is-open');
+    overlay.classList.remove('hidden');
+    setTimeout(() => {
+      mobileMenu.classList.remove('is-closing');
+    }, 250);
+  };
+
   const closeMenu = () => {
     mobileMenu.classList.add('is-closing');
     setTimeout(() => {
       mobileMenu.classList.remove('is-open', 'is-closing');
       overlay.classList.add('hidden');
     }, 250);
-  };
-
-  const openMenu = () => {
-    mobileMenu.classList.add('is-open');
-    overlay.classList.remove('hidden');
   };
 
   openMenuBtn.addEventListener('click', openMenu);
