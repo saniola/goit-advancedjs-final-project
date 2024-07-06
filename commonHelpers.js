cb66382327802d70b7e058c67c726af88da79f61
Netaly79 <48415878+Netaly79@users.noreply.github.com>
2024-07-06 16:46:41 +0000

    Deploying to gh-pages from @ saniola/goit-advancedjs-final-project@5ae5d8d4db3a506a01bcde12af48fb57dc900797 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import{f as e,a as t}from"./assets/fetch-and-set-quote-484e2d35.js";import"./assets/vendor-ae6d56ab.js";document.addEventListener("DOMContentLoaded",async()=>{e(),t({page:1})});document.querySelector(".toggle-btn-home").classList.remove("active");document.querySelector(".toggle-btn-favorites").classList.add("active");
+import{f as t,a as o}from"./assets/fetch-and-set-quote-b76b2dbf.js";import"./assets/vendor-ae6d56ab.js";window.onload=function(){const e=document.querySelector(".loader-text");e.style.display="none"};document.addEventListener("DOMContentLoaded",async()=>{t(),o({page:1})});document.querySelector(".toggle-btn-home").classList.remove("active");document.querySelector(".toggle-btn-favorites").classList.add("active");
 //# sourceMappingURL=commonHelpers.js.map
