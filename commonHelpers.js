fc8bd741d55434ca931c04ffb1b989fc132fb21f
ZorAnderius <119595649+ZorAnderius@users.noreply.github.com>
2024-07-06 14:25:12 +0000

    Deploying to gh-pages from @ saniola/goit-advancedjs-final-project@841da48aab485a0d8d9747ae6b72787d331363bf 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import{f as e,a as t}from"./assets/fetch-and-set-quote-096f01d0.js";import"./assets/vendor-ae6d56ab.js";document.addEventListener("DOMContentLoaded",async()=>{e(),t({page:1})});document.querySelector(".toggle-btn-home").classList.remove("active");document.querySelector(".toggle-btn-favorites").classList.add("active");
+import{f as e,a as t}from"./assets/fetch-and-set-quote-acb97427.js";import"./assets/vendor-ae6d56ab.js";document.addEventListener("DOMContentLoaded",async()=>{e(),t({page:1})});document.querySelector(".toggle-btn-home").classList.remove("active");document.querySelector(".toggle-btn-favorites").classList.add("active");
 //# sourceMappingURL=commonHelpers.js.map
